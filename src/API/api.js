aca2d0b7350b2c5ba7f26b085649e27dc2ccff30
SamruddhiPatole <samruddhipatole20@gmail.com>
2025-11-19 12:25:21 +0530

    added by samruddhi

--- src/API/api.js
+++ src/API/api.js
@@ -6,12 +6,7 @@
 // export const API_BASE_URL = "https://rg.157careers.in/api/ats/157industries";
 // export const CHAT_BASE_URL = "https://rg.157careers.in";
 
-<<<<<<< HEAD
 // export const API_BASE_PORTAL="http://192.168.1.42:8080/api/jobportal"
 // export const API_BASE_PORTAL="http://192.168.1.37:8080/api/jobportal"
-=======
-export const API_BASE_URL="http://192.168.1.44:9090/api/ats/157industries"
-
->>>>>>> 650ef088f79f3933e7e9fc9ed0e2d4284b264a55
 export const API_BASE_PORTAL="http://localhost:8080/api/jobportal"
 
